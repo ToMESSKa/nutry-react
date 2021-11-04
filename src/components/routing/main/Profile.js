8f883763622a40bda2b19c3b4831da4815b1e3ab
Kovacs Tamas <kovacstamas990@gmail.com>
2021-11-04 18:04:13 +0100

    Basics image upload page is created with no connection to backend

--- src/components/routing/main/Profile.js
+++ src/components/routing/main/Profile.js
@@ -1,10 +1,12 @@
 import {} from "antd";
 import React from "react";
 import {} from "react-router-dom";
+import ImageUploader from "../../routing/main/ImageUploader"
 
 function Profile({ children }) {
   return (
     <div className="profile">
+      <ImageUploader></ImageUploader>
       <p>Profile</p>
       {children}
     </div>
