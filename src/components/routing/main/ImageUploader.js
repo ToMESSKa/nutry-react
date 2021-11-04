31767aa19ef20dbcf514771ca71e6c8c1a46a703
Kovacs Tamas <kovacstamas990@gmail.com>
2021-11-04 22:18:55 +0100

    Picture can be uploaed in input form and sent to endpoint

--- src/components/routing/main/ImageUploader.js
+++ src/components/routing/main/ImageUploader.js
@@ -1,59 +1,46 @@
-import React from 'react';
-import ImageUploading from 'react-images-uploading';
+import React, { useState, useRef } from "react";
+import FileUploader from "./FileUploader";
+import axios from "axios";
 
-export function ImageUploader() {
-  const [images, setImages] = React.useState([]);
-  const maxNumber = 69;
 
-  const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
-    console.log(imageList, addUpdateIndex);
-    setImages(imageList);
+const ImageUploader = () => {
+
+
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileInput = useRef(null)
+
+  const onFileChange = (event) => {
+    console.log(event.target.files[0])
+    setSelectedFile(event.target.files[0]);
   };
+
+  const submitForm = () => {
+    console.log("hi")
+    const formData = new FormData();
+    console.log(formData)
+    formData.append("name", name);
+    formData.append("file", selectedFile);
+    axios
+      .post("http://localhost:8080/addprofilepicture", formData)
+      .then((res) => {
+        alert("File Upload success");
+      })
+      .catch((err) => alert("File Upload Error"));
+  };
+  
 
   return (
     <div className="App">
-      <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper">
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="100" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
-                  <button onClick={() => onImageRemove(index)}>Remove</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ImageUploading>
+      <div>
+                <input text="x" type="file" onChange={onFileChange} />
+                <button onClick={submitForm}>
+                  Upload!
+                </button>
+            </div>
     </div>
   );
-}
+
+};
 
 export default ImageUploader;
